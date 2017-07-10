33b19a73f155a5fb53e15ed34195b50581170352
Rocco Janse <rocco.janse@valtech.nl>
2017-07-10 12:36:12 +0200

    added scripts task using webpack

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,14 @@
 import config from '../../project.config.js';
 import path from 'path';
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import del from 'del';
+import webpackStream from 'webpack-stream';
+import webpack from 'webpack';
+import through from 'through2';
+import named from 'vinyl-named';
+import rename from 'gulp-rename';
+import sourcemaps from 'gulp-sourcemaps';
 
 /**
  * Cleans previously build scripts file.
@@ -21,11 +28,33 @@ function cleanScripts(done) {
  * @param {function} done Callback. 
  */
 function compileScripts(done) {
-    console.log('SCRIPTS!');
-    done();
+    gulp.src([path.join(config.folders.src.root, config.jsFileName + '.js')])
+        .pipe(named())
+        .pipe(webpackStream(require(path.join(config.cwd, 'webpack.config.js')), webpack, function(err, stats) {
+            if(err) throw new gutil.PluginError('webpack', err);        
+            if (config.debugMode) {
+                gutil.log(stats.toJson());
+            }
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.init({ 
+            loadMaps: true 
+        }))
+        .pipe(through.obj(function(file, enc, cb) {
+            // Dont pipe through any source map files as it will be handled
+            // by gulp-sourcemaps
+            var isSourceMap = /\.map$/.test(file.path);
+            if (!isSourceMap) this.push(file);
+            cb();
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.folders.build.js))
+        .on('finish', () => { done(); });
 }
 
 const scripts = gulp.series(cleanScripts, compileScripts);
-scripts.displayName = 'Compile scripts and modules.';
+scripts.displayName = 'Compile scripts and modules using webpack.';
 
 export default scripts;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,7 @@
-/* jshint esversion: 6 */
-var config = require('./project.config.js');
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const SvgStore = require('webpack-svgstore-plugin');
-const PermissionsOutputPlugin = require('webpack-permissions-plugin');
+import webpack from 'webpack';
+import ProgressBarPlugin from 'progress-bar-webpack-plugin';
 
 module.exports = {
-    entry: ['./src/index.js', './src/styles.scss'],
-    output: {
-        path: path.resolve(__dirname, config.folders.build.root),
-        filename: path.relative(config.folders.build.root, config.folders.build.js) + '/' + config.jsFileName + '.min.js'
-    },
     devtool: 'source-map',
     module: {
         rules: [
@@ -22,124 +9,16 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
+                    loader: 'babel-loader'//,
+                    // options: {
+                    //     presets: ['env']
+                    // }
                 }
-            },
-            { // regular css files
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: 'style-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                require('autoprefixer')(),
-                                require('cssnano')({
-                                    discardComments: {
-                                        removeAll: true
-                                    },
-                                    zindex: false,
-                                    discardUnused: false,
-                                    mergeIdents: false,
-                                    reduceIdents: false,
-                                    safe: true,
-                                    sourcemap: true
-                                })
-                            ]
-                        }
-                    }]
-                })
-            },
-            { // sass / scss
-                test: /\.(sass|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                require('autoprefixer')(),
-                                require('cssnano')({
-                                    discardComments: {
-                                        removeAll: true
-                                    },
-                                    zindex: false,
-                                    discardUnused: false,
-                                    mergeIdents: false,
-                                    reduceIdents: false,
-                                    safe: true,
-                                    sourcemap: true
-                                })
-                            ]
-                        }
-                    }, {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin({
-            filename: path.relative(config.folders.build.root, config.folders.build.css) + '/' + config.cssFileName + '.min.css',
-            allChunks: true
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            beautify: false,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                screw_ie8: true
-            },
-            comments: false,
-            sourceMap: true
-        }),
-        new SvgStore(path.resolve(__dirname, config.folders.src.root, '**/*.svg'), path.resolve(__dirname, config.folders.build.root), {
-            name: 'sprite.svg'
-            //prefix: 'myprefix-',
-            // svgoOptions: {
-            //     // options for svgo, optional
-            // }
-        }),
-        new CopyWebpackPlugin([{
-            from: config.folders.src.assets.root,
-            to: config.folders.build.assets.root,
-            ignore: [path.relative(config.folders.src.assets.root, path.join(config.folders.src.assets.icons.svg, '**/*'))],
-        }], {
-            copyUnmodified: true
-        }),
-        new ImageminPlugin({ test: /\.(jpe?g|png|gif)$/i }),
-        new PermissionsOutputPlugin({
-            buildFolders: [{
-                path: path.resolve(config.folders.build.root, config.folders.build.assets.root + '/'), 
-                fileMode: '777',
-                dirMode: '777'
-            }]
-        }),
+        new webpack.optimize.UglifyJsPlugin(),
         new ProgressBarPlugin()
     ]
 };
